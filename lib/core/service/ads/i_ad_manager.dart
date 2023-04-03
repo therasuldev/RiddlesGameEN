faabfada01170bd8df10625d2b1cfd923e0fb433
therasuldev <rasul.ramixanov@gmail.com>
2023-04-03 16:18:40 +0400

    removed rewarded ad

--- lib/core/service/ads/i_ad_manager.dart
+++ lib/core/service/ads/i_ad_manager.dart
@@ -1,9 +1,4 @@
-import 'package:flutter/material.dart';
-
 abstract class IAdManager {
-  void loadRewardedAd();
-  void showRewardedAd(BuildContext context);
-
   void loadInterstitialAd();
   void showInterstitialAd();
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
